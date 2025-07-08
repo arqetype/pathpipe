96f110ba156603effb1f8438c10423196b732da5
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-07-08 16:11:35 +0200

    feat(swagger): add swagger document to the api route

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
+
   app.use(cookieParser());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,6 +21,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Weaver Backend API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, documentConfig);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.NEST_PORT ?? 4000);
 }
 void bootstrap();
