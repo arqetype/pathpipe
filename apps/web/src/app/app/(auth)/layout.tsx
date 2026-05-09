54cea024171a480d287a7cd223350d3f58ef3d99
Clément Omnès <clement.mns.pro@gmail.com>
2026-05-09 18:21:30 +0200

    style(auth): add dark background for auth layout

--- apps/web/src/app/app/(auth)/layout.tsx
+++ apps/web/src/app/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthenticationLayout({
   children,
 }: AuthenticationLayoutProps) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-muted/50 p-4">
+    <div className="flex min-h-screen items-center justify-center dark:bg-background bg-muted/40 p-4">
       <div className="w-full max-w-4xl space-y-4">{children}</div>
     </div>
   );
